191c0b40e55d8ff977af71b0affc399c066b717c
jenxjennx <76090910+JenxJennx@users.noreply.github.com>
2022-05-12 16:26:37 -0500

    Inputs and basics

---
+++ rockPaperScissors.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class rockPaperScissors { //public class of same name
+    public static void main(String[] args) { //main() method}
+        Scanner scanner = new Scanner(System.in); //declare scanner object for user input
+    //Decoreate the program
+        System.out.println("|||||||||||||||||||||||||||||||||||||"); //Print boundary
+        System.out.println("|-Let's play Rock, Paper, Scissors!-|"); //Welcome player message
+    //Ask for user input
+        System.out.println("|||||||||||||||||||||||||||||||||||||"); //Print boundary
+        System.out.println(" ||Rock, Paper, Scissors, Shoot!||"); //Make call to enter choice
+        String playerChoice = scanner.nextLine(); //Store the okayers move}
+    //Get computer move by random number selection
+
+    Random random = new Random(); //Declare random object
+    int computerChoice = random.nextInt(3); //Store the computers move in an int variable of random number between 0 and 2
+
+    //Print the cmputer move vs the players move, show based on conditions
+   String computerMove = "";
+
+    if (computerChoice == 0){
+        computerMove = "Rock";
+    }
+    else if (computerChoice == 1){
+        computerMove = "Paper";
+    }
+    else if (computerChoice == 2){
+        computerMove = "Scissors";
+    }
+    //Print the versus and the winner
+    System.out.println("|||||||||||||||||||||||||||||||||||||"); //Print boundary
+    System.out.println("||" + playerChoice + " - VERSUS - " + computerMove + "||");
+
+    }
+    
+}
